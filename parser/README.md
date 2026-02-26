8a6f5a865f03afae1d7d4fa5a0c853efbcc9f04f
memepolicedogo <finseyconrymurray@gmail.com>
2026-02-26 15:21:11 -0500

    Final touches

--- parser/README.md
+++ parser/README.md
@@ -2,9 +2,18 @@
 Creates intermediate JSON representations of instruction tables from the Intel SDM PDFs.\
 These tables are joined across pages so each instruction should have a single instruction table and a single operand table, this is a pretty basic script so artifacts owing to inconsistancies in Intel's documentation or limitations of the PDF reader aren't normalized out.\
 The input PDFs should only be the instruction definitions from the SDM (e.g. pgs. 119-697 from Vol 2A), parsing the full volume may cause unexpected behavior and will slow down the process significantly.\
-You can use the combined reference & a single PDF, but each PDF is parsed on a seperate thread so it should run faster using seperate PDFs for each volume. If for some reason you really want to optimize parsing speed you can split the PDFs further, as long as you don't break up an instruction (e.g. if an instruction table is on page 123 but the opcode table is on page 124 if you split the pdf into 1-123 and 124-n later proccess won't work properly)
 ## Usage
+Accepts paths to either specific PDFs or directories, non PDFs (based on extension) in given directories will be ignored
 ```
 ./parsepdfs.py {paths to PDF(s)...}
 ```
 For each PDF it produces a `{PDF Name}.json` file in `./out/`
+## Optimization
+Each PDF is parsed in a seperate process, so splitting up the larger PDFs will allow you to take advantage of your (presumably) multi core CPU. It likely isn't worth the time to split up the PDFs more than the 4 parts Intel provides, especily because you have to be careful not to split any instruction/opperand tables. That said, I'm probably gonna implement automatic optimization of this kind in the future because it'd be fun.
+# Merging
+This is a super simple script that just takes the JSON generated in `./out/` and combines them into a single JSON file for portability.
+## Usage
+```
+./mergejson.py
+```
+Make sure you run it in the same directory as you ran `./parsepdfs.py`, NOT in `./out`. 
--- parser/parsepdfs.py
+++ parser/parsepdfs.py
@@ -3,7 +3,7 @@ import pdfplumber
 import sys
 import os
 import json
-import threading
+import multiprocessing
 
 def type_table(table):
     match table[0][0][0:5]:
@@ -19,7 +19,8 @@ def parse(file):
     pdf = pdfplumber.open(file);
     # get base name for JSON output
     filename = file.split('/')[-1]
-    filename.replace('.pdf', '')
+    filename = filename.replace('.pdf', '')
+    print(f"{filename}: " + "Parsing PDF")
     full_data = []
     for page in pdf.pages:
         tables = page.extract_tables()
@@ -31,43 +32,75 @@ def parse(file):
                 full_data.append(table)
     # Combine extended tables in full_data and link instruction tables to operand encoding
     pdf.close()
+    raw_tables = len(full_data)
+    print(f"{filename}: " + f"Extracted {raw_tables} tables ")
+    print(f"{filename}: " + "Cleaning and combining tables")
+    unknown_count = 0
     cleaned = []
     prev = None
-    for table in full_data:
+    for table in full_data: 
         curr = type_table(table)
-        if curr == 'Unknown':
-            print("--UNKNOWN--")
+        if curr == 'Unknown': 
+            unknown_count += 1
+            print(f"{filename}: " + "--UNKNOWN--")
+            print(f"{filename}: ", end='') 
             print(table)
-            print("-----------")
+            print(f"{filename}: " + "-----------")
             # Skip unknown tables
             prev = None
             curr = None
             continue
-        if curr == prev:
+        if curr == prev: 
             # Remove defenition from table
             table.pop(0)
             # Merge tables
             cleaned[-1].extend(table)
-        else:
+        else: 
             cleaned.append(table)
         prev = curr
+    print(f"{filename}: " + "---STATS---")
+    print(f"{filename}: " + f"Unique/Total: {len(cleaned)}/{raw_tables}")
+    print(f"{filename}: " + f"Unknown Tables: {unknown_count}")
+    # total tables - tables ignored - final table count = tables that were merged into others
+    print(f"{filename}: " + f"Merges: {raw_tables-unknown_count-len(cleaned)}")
+    print(f"{filename}: " + f"Instructions: {int(len(cleaned)/2)}")
+    print(f"{filename}: " + "-----------")
+    print(f"{filename}: " + f"Writing JSON results to {filename}.json")
     json.dump(cleaned, open(f"out/{filename}.json", 'w'))
+    print(f"{filename}: " + "Exiting")
     return
 
 def main(args):
     if len(args) == 1:
         print("Path(s) to PDF(s) required")
     else:
+        # Build list of PDFs
+        pdfs = set()
+        for arg in args[1:]:
+            if not os.path.exists(arg):
+                print(f"Invalid argument: {arg}")
+                return
+            path = os.path.abspath(arg)
+            if os.path.isdir(path):
+                for file in os.listdir(path):
+                    file = path+'/'+file
+                    if os.path.isfile(file) and file.split('.')[-1] == 'pdf':
+                        pdfs.add(file)
+            elif path.split('.')[-1] != 'pdf':
+                print(f"Invalid file type: {arg}")
+            else:
+                pdfs.add(path)
         # Create output dir
         os.makedirs('out', exist_ok=True)
         # Each PDF is wholly independent so they can each be processed in a unique thread
-        threads = []
-        for arg in args[1:]:
-            threads.append(threading.Thread(target=parse, args=(arg)))
-        for thread in threads:
-            thread.start()
-        for thread in threads:
-            thread.join()
+        procs = []
+        for pdf in pdfs:
+            procs.append(multiprocessing.Process(target=parse, args=(pdf,)))
+        for proc in procs:
+            proc.start()
+        for proc in procs:
+            # Wait for processing to end
+            proc.join()
     return
 if __name__ == "__main__":
     main(sys.argv)
